2e46a42761f0f67b3ca8385ac1c2f96837a8b9bb
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-28 22:40:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const express = require('express');
 //set up expressapp
 const app = express();
 
+app.get('/api', function(req, res) {
+  console.log('GET request');
+  res.send({name:'Yoshi'});
+});
+
+
 //listen for requests
 app.listen(process.env.port||4000,function() {
   console.log('now listening for requests');
