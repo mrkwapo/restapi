14d1a35270b77683bc61fa1e845d5b7dee600bb6
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-28 23:59:18 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 //set up expressapp
 const app = express();
+
+app.use(bodyParser.json());
 
 //initialize routes
 app.use('/api', require('./routes/api'));
