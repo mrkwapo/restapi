71ade6230288ec297701723fa6c2a6ea7a300a3d
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-28 23:22:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require('express');
 //set up expressapp
 const app = express();
 
-app.get('/api', function(req, res) {
-  console.log('GET request');
-  res.send({name:'Yoshi'});
-});
-
+//initialize routes
+app.use('/api', require('./routes/api'));
 
 //listen for requests
 app.listen(process.env.port||4000,function() {
   console.log('now listening for requests');
+  console.log('nodemon working');
+
 });
