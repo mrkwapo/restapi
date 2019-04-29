d2a45737f4fda1a11856b0d31050ddb3c3d5cb42
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-29 12:54:50 -0400

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -3,25 +3,25 @@ const router = express.Router();
 const User = require('../models/user');
 
 //get a list of users from the db
-router.get('/users', function(req,res){
+router.get('/users', function(req,res,next){
   res.send({type: 'GET'});
 });
 
 //add a new user to the db
-router.post('/users', function(req,res){
+router.post('/users', function(req,res,next){
 User.create(req.body).then(function(user) {
   res.send(user);
-  });
+}).catch(next);
 });
 
 
 //update  a user to the db
-router.put('/users/:id', function(req,res){
+router.put('/users/:id', function(req,res,next){
   res.send({type: 'PUT'});
 });
 
 //delete a user from the db
-router.delete('/users/:id', function(req,res){
+router.delete('/users/:id', function(req,res,next){
   res.send({type: 'DELETE'});
 });
 
