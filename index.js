5aeb13a897ac7dc5f12113bd03e6c57e866b92fc
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-29 12:55:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -14,9 +14,15 @@ app.use(bodyParser.json());
 //initialize routes
 app.use('/api', require('./routes/api'));
 
+//error handling middleware
+app.use(function(err,req,res,next){
+  // console.log(err);
+res.status(422).send({error: err.message});
+})
+
 //listen for requests
 app.listen(process.env.port||4000,function() {
   console.log('now listening for requests');
-  console.log('nodemon working');
+  console.log('nodemon working again');
 
 });
