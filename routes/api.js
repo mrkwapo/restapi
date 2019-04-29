1c314f30cd2e73863bcefc558af74184723accd0
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-28 23:23:23 -0400

    Add files via upload

---
+++ routes/api.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+
+//get a list of users from the db
+router.get('/users', function(req,res){
+  res.send({type: 'GET'});
+});
+
+//add a new user to the db
+router.post('/users', function(req,res){
+  res.send({type: 'POST'});
+});
+
+
+//update  a user to the db
+router.put('/users/:id', function(req,res){
+  res.send({type: 'PUT'});
+});
+
+//delete a user from the db
+router.delete('/users/:id', function(req,res){
+  res.send({type: 'DELETE'});
+});
+
+
+module.exports= router;
