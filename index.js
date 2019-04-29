3acab4b6e6a3dc35f4881c30b0273f1b99d17a48
mrkwapo <48810912+mrkwapo@users.noreply.github.com>
2019-04-29 17:33:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,27 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-//set up expressapp
+// set up express app
 const app = express();
 
-//connect to mongodb
+// connect to mongodb
 mongoose.connect('mongodb://localhost/ninjago');
 mongoose.Promise = global.Promise;
 
+//set up static files
+app.use(express.static('public'));
+
+// use body-parser middleware
 app.use(bodyParser.json());
 
-//initialize routes
+// initialize routes
 app.use('/api', require('./routes/api'));
 
-//error handling middleware
-app.use(function(err,req,res,next){
-  // console.log(err);
-res.status(422).send({error: err.message});
-})
+// error handling middleware
+app.use(function(err, req, res, next){
+    console.log(err); // to see properties of message in our console
+    res.status(422).send({error: err.message});
+});
 
-//listen for requests
-app.listen(process.env.port||4000,function() {
-  console.log('now listening for requests');
-  console.log('nodemon working again');
-
+// listen for requests
+app.listen(process.env.port || 4000, function(){
+    console.log('now listening for requests');
 });
